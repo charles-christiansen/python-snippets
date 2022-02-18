6adc20dad16e3cf9157e233c522dd44036abdb96
Charles Christiansen <charlesachristiansen@gmail.com>
2022-02-17 22:17:00 -0500

    Signed integer reversal, with comment

--- reverseInt.py
+++ reverseInt.py
@@ -1,3 +1,8 @@
+## Remove Value from List
+## LeetCode: https://leetcode.com/problems/reverse-integer/
+## Given a signed 32-bit integer, reverse the digits and return
+## the resulting integer. If the result is out of bounds of
+## a 32-bit integer, return 0.
 class Solution:
     def reverse(self, x: int) -> int:
         digits=list(reversed([digit for digit in str(x)]))
